6f9649b2dd3d9b11451dca7679b40fda61af861b
Hyungro Lee <hroe.lee@gmail.com>
2021-02-27 16:27:06 -0500

    minor

--- host.py
+++ host.py
@@ -10,14 +10,14 @@ class Host(object):
         self.get_argparse()
 
     def status(self, interval=None, times=None):
-        interval = interval or self.interval
-        times = times or self.times
+        interval = interval or self.args.interval
+        times = times or self.args.times
         for i in range(times):
             self.process.processes()
             self.system.cpus()
             self.system.memory()
-            self.process.save_all()
-            self.system.save_all()
+            self.process.save_all(self.args.output_path)
+            self.system.save_all(self.args.output_path)
             time.sleep(interval)
 
     def get_argparse(self):
@@ -26,8 +26,6 @@ class Host(object):
         parser.add_argument("--times", default=5)
         parser.add_argument("--output_path")
         args = parser.parse_args()
-        self.interval = args.interval
-        self.times = args.times
         self.args = args
           
 if __name__ == "__main__":
